5fe44c54d87dab5500cfafbfd528e1b29f1c5745
xtofl <kristoffel.pirard@gmail.com>
2018-12-16 21:12:26 +0100

    play with `concept`.

--- src/meetingcpp-2018-report/concepts.cxx
+++ src/meetingcpp-2018-report/concepts.cxx
@@ -1,24 +1,22 @@
 #include <cstdio>
+#include <vector>
+#include <functional>
+#include <memory>
 
 template<typename T>
-concept bool Monoid = requires(T a, T b) {
-    {T::empty()} -> T
+concept Monoid = requires (T a, T b, T c) {
+    {T::identity}; // has identity
+    {a + b} -> T; // says nothing about the meaning
+    {(a + b) + c == a + (b + c)}
 };
 
-auto start(Monoid m) {
-    return Monoid::empty()
-}
 
-auto acc(Monoid m...) {
-    auto zero = Monoid::empty();
-    return zero.append(m)...;
+template<Monoid T>
+auto sum(T... ms) {
+    return Monoid::zero + ms ...;
 }
-
-struct X {
-    static X empty() { return X{}; }
-};
 
 int main() {
-    auto x = start(X{});
-    // FAILS... auto y = start(1);
+    auto as = std::vector<A>{1, 2, 3};
+    auto as2 = as;
 }
