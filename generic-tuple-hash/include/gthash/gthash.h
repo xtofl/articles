ba90e2f2ddc9619fd4e0134fc1929914c48eb11b
xtofl <kristoffel.pirard@gmail.com>
2020-05-05 17:38:56 +0200

    Don't use constexpr hashing

--- generic-tuple-hash/include/gthash/gthash.h
+++ generic-tuple-hash/include/gthash/gthash.h
@@ -1,12 +1,17 @@
 #pragma once
 
 #include <tuple>
+#include <functional>
 #include <cstddef>
 
 namespace gthash {
 namespace lowlevel {
+
     template<typename T>
-    constexpr size_t hash(T &&t){ return t; }
+    constexpr size_t hash(T t){ return std::hash<T>{}(t); }
+
+    template<typename T>
+    constexpr size_t hash(const T *t){ return std::hash<const T*>{}(t); }
 
     constexpr size_t hash(std::tuple<>) {
         return 1;
--- generic-tuple-hash/tests.cpp
+++ generic-tuple-hash/tests.cpp
@@ -6,21 +6,23 @@
 using namespace gthash;
 
 TEST(low_level, same_value_results_in_same_hash) {
-    static_assert(lowlevel::hash(2) == lowlevel::hash(2));
+    EXPECT_EQ(lowlevel::hash(2), lowlevel::hash(2));
+    EXPECT_EQ(lowlevel::hash('2'), lowlevel::hash('2'));
 }
 
 TEST(low_level, different_value_results_in_different_hash) {
-    static_assert(lowlevel::hash(1) != lowlevel::hash(2));
+    EXPECT_NE(lowlevel::hash(1), lowlevel::hash(2));
+    EXPECT_NE(lowlevel::hash('1'), lowlevel::hash('2'));
 }
 
 TEST(low_level, empty_tuple_hashes_to_same_value_everytime) {
     constexpr std::tuple<> empty;
-    static_assert(lowlevel::hash(empty) == lowlevel::hash(empty));
+    EXPECT_EQ(lowlevel::hash(empty), lowlevel::hash(empty));
     EXPECT_EQ(lowlevel::hash(empty), lowlevel::hash(empty));
 }
 
 TEST(low_level, singleton_tuple_hashes_to_element_hash) {
     constexpr auto value = 1;
-    static_assert(lowlevel::hash(value) == lowlevel::hash(std::make_tuple(1)));
-    static_assert(lowlevel::hash(value + 1) != lowlevel::hash(std::make_tuple(1)));
+    EXPECT_EQ(lowlevel::hash(value), lowlevel::hash(std::make_tuple(1)));
+    EXPECT_NE(lowlevel::hash(value + 1), lowlevel::hash(std::make_tuple(1)));
 }
