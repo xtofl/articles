2d14f6cb78b60769111dbc52c1aa782b98efb04e
xtofl <kristoffel.pirard@gmail.com>
2020-05-05 19:16:53 +0200

    Remove non-requirement test

--- generic-tuple-hash/tests.cpp
+++ generic-tuple-hash/tests.cpp
@@ -21,12 +21,6 @@ TEST(low_level, empty_tuple_hashes_to_same_value_everytime) {
     EXPECT_EQ(lowlevel::hash(empty), lowlevel::hash(empty));
 }
 
-TEST(low_level, singleton_tuple_hashes_to_element_hash) {
-    constexpr auto value = 1;
-    EXPECT_EQ(lowlevel::hash(value), lowlevel::hash(std::make_tuple(1)));
-    EXPECT_NE(lowlevel::hash(value + 1), lowlevel::hash(std::make_tuple(1)));
-}
-
 TEST(composed_free_function, same_value_results_in_same_hash) {
     constexpr auto value = std::make_tuple(1, true);
     EXPECT_EQ(lowlevel::hash(value), lowlevel::hash(value));
